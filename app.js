6a15b4ad71a73dfd433c2483a8f378dd7826a354
Paulo Andre Melo Costa <pauloamcosta2@gmail.com>
2019-03-24 14:57:19 -0300

    package.json

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 routes(app);
 
-var server = app.listen(8080, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
     console.log("app running on port.", server.address().port);
 });
