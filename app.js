f35e4a55e2f07492ffd6fb4038a57ac56cebf639
Paulo Andre Melo Costa <pauloamcosta2@gmail.com>
2019-03-24 14:44:02 -0300

    package.json add

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var routes = require("./routes/routes.js");
+var routes = require("./routes");
 var app = express();
 
 app.use(bodyParser.json());
@@ -8,6 +8,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 routes(app);
 
-var server = app.listen(3000, function () {
+var server = app.listen(5000, function () {
     console.log("app running on port.", server.address().port);
 });
